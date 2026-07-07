259854c4e3ac41d86bc7f3a8e16dec35ab934afc
berishaajshe07-commits <berishaajshe07@gmail.com>
2026-07-07 19:13:49 +0000

    BMI calculator final

--- app.py
+++ app.py
@@ -9,13 +9,17 @@ st.title("BMI Calculator")
 # MongoDB connection
 uri = st.secrets["MONGO_URI_ST"]
 
-server = MongoClient(uri)
-server.admin.command("ping")
+client = MongoClient(uri)
 
-db = server["sample_mfix"]
-collection = db["bmi_records"]
+# Check connection
+client.admin.command("ping")
 
 st.success("DB connection done!")
+
+
+# Database and new collection
+db = client["sample_mfix"]
+collection = db["bmi_records"]
 
 
 # Inputs
@@ -23,39 +27,45 @@ name = st.text_input("Name")
 
 height = st.number_input(
     "Height (meters)",
-    min_value=0.1
+    min_value=0.1,
+    step=0.01
 )
 
 weight = st.number_input(
     "Weight (kg)",
-    min_value=1.0
+    min_value=1.0,
+    step=0.1
 )
 
 
 # Button
 if st.button("Calculate BMI"):
 
-    # BMI calculation
-    bmi = weight / (height ** 2)
+    if name and height and weight:
+
+        # BMI calculation
+        bmi = weight / (height ** 2)
+
+        # Show outputs in Streamlit
+        st.subheader("Results")
+
+        st.write("Name:", name)
+        st.write("Height:", height)
+        st.write("Weight:", weight)
+        st.write("BMI:", round(bmi, 2))
 
 
-    # Outputs in Streamlit
-    st.subheader("Results")
+        # Save in MongoDB
+        record = {
+            "name": name,
+            "height": height,
+            "weight": weight,
+            "bmi": round(bmi, 2)
+        }
 
-    st.write("Name:", name)
-    st.write("Height:", height)
-    st.write("Weight:", weight)
-    st.write("BMI:", round(bmi, 2))
+        collection.insert_one(record)
 
+        st.success("Data registered in MongoDB!")
 
-    # Register in MongoDB
-    data = {
-        "name": name,
-        "height": height,
-        "weight": weight,
-        "bmi": round(bmi, 2)
-    }
-
-    collection.insert_one(data)
-
-    st.success("Data saved in MongoDB!")
+    else:
+        st.warning("Please fill all fields")
